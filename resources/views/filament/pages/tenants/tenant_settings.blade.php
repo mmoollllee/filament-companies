e17c7c05dab583471fedfa3c2c3538600689f1d8
MGraf <info@moritz-graf.de>
2025-10-06 00:16:23 +0200

    Gate tenant_settings

--- resources/views/filament/pages/tenants/tenant_settings.blade.php
+++ resources/views/filament/pages/tenants/tenant_settings.blade.php
@@ -1,20 +1,20 @@
 <x-filament-panels::page>
+@if (Gate::check('update', $tenant))
     <x-filament-panels::form id="form" wire:submit="save" class="gap-y-1">
         {{ $this->form }}
 
-        @if (Gate::check('update', $tenant))
-            <div class="text-right">
-                <x-filament::button type="submit">
-                    {{ __('filament-tenants::default.buttons.save') }}
-                </x-filament::button>
-            </div>
-        @endif
+        <div class="text-right">
+            <x-filament::button type="submit">
+                {{ __('filament-tenants::default.buttons.save') }}
+            </x-filament::button>
+        </div>
     </x-filament-panels::form>
     
     @livewire(\Wallo\FilamentTenants\Http\Livewire\TenantEmployeeManager::class, compact('tenant'))
-
+    
     @if (!$tenant->personal_tenant && Gate::check('delete', $tenant))
-        <x-filament-tenants::section-border />
-        @livewire(\Wallo\FilamentTenants\Http\Livewire\DeleteTenantForm::class, compact('tenant'))
+    <x-filament-tenants::section-border />
+    @livewire(\Wallo\FilamentTenants\Http\Livewire\DeleteTenantForm::class, compact('tenant'))
     @endif
+@endif
 </x-filament-panels::page>
